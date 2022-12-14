423d1a6efd53b222f6ef94a0003a90937051ea30
spencer-hurd <schurd02@gmail.com>
2022-12-14 14:27:57 -0600

    end of day 1

--- db/seed.js
+++ db/seed.js
@@ -1,20 +1,26 @@
-const { client, getAllUsers, createUser } = require("./index");
+const { client, getAllUsers, createUser, updateUser } = require("./index");
 
 async function createInitialUsers() {
   try {
     console.log("Starting to create users...");
 
-    const albert = await createUser({
+    await createUser({
       username: "albert",
       password: "bertie99",
+      name: "Albert Palbert",
+      location: "San Francisco",
     });
-    const sandra = await createUser({
+    await createUser({
       username: "sandra",
       password: "2sandy4me",
+      name: "Sandra Shells",
+      location: "Atlantic City",
     });
-    const glamgal = await createUser({
+    await createUser({
       username: "glamgal",
       password: "soglam",
+      name: "Paris Hilton",
+      location: "Beverly Hills",
     });
 
     console.log("Finished creating users!");
@@ -44,7 +50,10 @@ async function createTables() {
     CREATE TABLE users (
         id SERIAL PRIMARY KEY,
         username varchar(255) UNIQUE NOT NULL,
-        password varchar(255) NOT NULL
+        password varchar(255) NOT NULL,
+        name VARCHAR(255) NOT NULL,
+        location VARCHAR(255) NOT NULL,
+        active BOOLEAN DEFAULT true
       );
       `);
     console.log("Finished building tables!");
@@ -70,8 +79,16 @@ async function testDB() {
   try {
     console.log("Starting to test database...");
 
+    console.log("Calling getAllUsers");
     const users = await getAllUsers();
-    console.log("getAllUsers: ", users);
+    console.log("Result: ", users);
+
+    console.log("Calling updateUser on users[0]");
+    const updateUserResult = await updateUser(users[0].id, {
+      name: "John Doe",
+      location: "Boston",
+    });
+    console.log("Result: ", updateUserResult);
     console.log("Finished database tests!");
   } catch (error) {
     console.error("Error testing database!");
