30f615709ca057222a9ca3aee9753f439045907a
spencer-hurd <schurd02@gmail.com>
2022-12-21 14:22:50 -0600

    requireUser fixes

--- api/utils.js
+++ api/utils.js
@@ -2,14 +2,15 @@ function requireUser(req, res, next) {
   if (!req.user) {
     next({
       name: "MissingUserError",
-      message: "You must be logged in to perform this action",
+      message: "You must be logged in to perform this action.",
     });
   }
 
   next();
 }
 
-function requireActiveUser(req, res, next) {
+async function requireActiveUser(req, res, next) {
+  await requireUser;
   if (!req.user.active) {
     next({
       name: "InactiveUserError",
